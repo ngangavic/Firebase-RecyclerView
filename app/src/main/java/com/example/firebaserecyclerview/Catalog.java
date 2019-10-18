cf7bb5bcc2070a6f89e94cbf2ba96ca8e7e9d185
ngangavic <ngangavictor10@gmail.com>
2019-10-18 14:59:58 +0300

    catalog row

---
+++ app/src/main/java/com/example/firebaserecyclerview/Catalog.java
@@ -0,0 +1,25 @@
+package com.example.firebaserecyclerview;
+
+public class Catalog {
+
+    String name;
+    String code;
+    String price;
+    String qty;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getQty() {
+        return qty;
+    }
+}
