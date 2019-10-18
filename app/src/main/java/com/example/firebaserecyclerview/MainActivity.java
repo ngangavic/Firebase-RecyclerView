4714e9e2b857155e1c17fb5bb09c2f3fa6e31062
ngangavic <ngangavictor10@gmail.com>
2019-10-18 14:59:23 +0300

    main activity

--- app/src/main/java/com/example/firebaserecyclerview/MainActivity.java
+++ app/src/main/java/com/example/firebaserecyclerview/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.firebaserecyclerview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private RecyclerViewAdapter recyclerViewAdapter;
+    private DatabaseReference databaseReference;
+    private List<Catalog> catalogList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        catalogList = new ArrayList<Catalog>();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("users").child("IBAipw8FTIPYgjvNDyEiT03HAsu2").child("catalog");
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                    Catalog catalog = snapshot.getValue(Catalog.class);
+
+                    catalogList.add(catalog);
+
+                }
+                recyclerViewAdapter = new RecyclerViewAdapter(MainActivity.this, catalogList);
+                recyclerView.setAdapter(recyclerViewAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
+
 }
